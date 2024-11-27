3127e5ad2c70f10c6eb61279129d73083010c749
Vita Midori <hi@vitamidori.com>
2024-11-27 11:14:48 +0100

    Add a script for downloading the embeddings cache easily

--- research/lib/data_access.py
+++ research/lib/data_access.py
@@ -10,12 +10,9 @@ import pandera as pa
 from demokratis_ml.data import schemata
 
 
-def ensure_dataframe_is_available(local_path: pathlib.Path) -> None:
-    """Download a dataframe from an Exoscale Simple Object Storage bucket if it is not already available locally."""
-    logger = logging.getLogger("ensure_dataframe_is_available")
-    if local_path.exists():
-        logger.info("File %s already exists locally.", local_path)
-        return
+def download_file_from_exoscale(remote_path: pathlib.Path, local_path: pathlib.Path) -> None:
+    """Download an arbitrary file from our Exoscale Simple Object Storage bucket."""
+    logger = logging.getLogger("download_file_from_exoscale")
     s3 = boto3.client(
         "s3",
         aws_access_key_id=os.environ["EXOSCALE_SOS_ACCESS_KEY"],
@@ -23,10 +20,19 @@ def ensure_dataframe_is_available(local_path: pathlib.Path) -> None:
         endpoint_url=os.environ["EXOSCALE_SOS_ENDPOINT"],
     )
     bucket_name = os.environ["EXOSCALE_SOS_BUCKET"]
-    remote_path = pathlib.Path("dataframes") / local_path.name
+    # remote_path = pathlib.Path("dataframes") / local_path.name
     local_path.parent.mkdir(parents=True, exist_ok=True)
-    logger.warning("Downloading %s from bucket %s to %s", remote_path, bucket_name, local_path)
+    logger.info("Downloading %s from bucket %s to %s", remote_path, bucket_name, local_path)
     s3.download_file(bucket_name, str(remote_path), local_path)
+
+
+def ensure_dataframe_is_available(local_path: pathlib.Path) -> None:
+    """Download a dataframe from our Exoscale Simple Object Storage bucket if it is not already available locally."""
+    logger = logging.getLogger("ensure_dataframe_is_available")
+    if local_path.exists():
+        logger.info("File %s already exists locally.", local_path)
+        return
+    download_file_from_exoscale(pathlib.Path("dataframes") / local_path.name, local_path)
 
 
 @pa.check_output(schemata.FullConsultationDocumentSchemaV1.to_schema())
