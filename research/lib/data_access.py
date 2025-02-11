cf956a166d37c5211b20ea259e353d77b3befee1
Vita Midori <hi@vitamidori.com>
2025-02-11 14:35:54 +0100

    Speed up data loading 5x by using pyarrow filters and especially by disabling MLflow dataset logging

--- research/lib/data_access.py
+++ research/lib/data_access.py
@@ -1,6 +1,8 @@
 """Utilities for selecting and loading input data."""
 
+import functools
 import logging
+import operator
 import os
 import pathlib
 from collections.abc import Iterable
@@ -8,7 +10,9 @@ from typing import Any
 
 import boto3
 import pandas as pd
-import pandera as pa
+import pandera
+import pyarrow as pa
+import pyarrow.compute
 
 from demokratis_ml.data import schemata
 
@@ -38,7 +42,7 @@ def ensure_dataframe_is_available(local_path: pathlib.Path) -> None:
     download_file_from_exoscale(pathlib.Path("dataframes") / local_path.name, local_path)
 
 
-@pa.check_output(schemata.FullConsultationDocumentSchemaV1.to_schema())
+@pandera.check_output(schemata.FullConsultationDocumentSchemaV1.to_schema())
 def load_consultation_documents(  # noqa: PLR0913
     input_file: pathlib.Path,
     *,
@@ -50,24 +54,27 @@ def load_consultation_documents(  # noqa: PLR0913
 ) -> pd.DataFrame:
     """Load and filter consultation documents from a parquet file.
 
-    If an MLflow client is provided, the loaded dataset is logged as an input artifact.
+    ~~If an MLflow client is provided, the loaded dataset is logged as an input artifact.~~ (MLflow logging
+    is currently disabled because it's slow.)
     """
-    # Read
     assert input_file.suffix == ".parquet", f"Expected a .parquet file, got {input_file}"
-    df = pd.read_parquet(input_file)
-    # Filter
+    filters = []
     if only_document_sources is not None:
-        df = df[df["document_source"].isin(only_document_sources)]
+        filters.append(pyarrow.compute.is_in(pyarrow.compute.field("document_source"), pa.array(only_document_sources)))
     if only_languages is not None:
-        df = df[df["document_language"].isin(only_languages)]
+        filters.append(pyarrow.compute.is_in(pyarrow.compute.field("document_language"), pa.array(only_languages)))
     if only_doc_types is not None:
         assert set(only_doc_types) - {None} <= schemata.DOCUMENT_TYPES, f"Unknown doc types: {only_doc_types}"
-        df = df[df["document_type"].isin(only_doc_types)]
+        filters.append(pyarrow.compute.is_in(pyarrow.compute.field("document_type"), pa.array(only_doc_types)))
     if starting_year is not None:
-        df = df[df["consultation_start_date"].dt.year >= starting_year]
-    # Log to MLflow
-    if mlflow is not None:
-        name = input_file.name.replace(".parquet", "")
-        dataset = mlflow.data.from_pandas(df, source=input_file, name=name)
-        mlflow.log_input(dataset)
+        filters.append(pyarrow.compute.year(pyarrow.compute.field("consultation_start_date")) >= starting_year)
+
+    filters_composed = functools.reduce(operator.and_, filters) if filters else None
+    df = pd.read_parquet(input_file, filters=filters_composed)
+
+    # Log to MLflow - this is disabled because it takes 40+ seconds :/
+    # if mlflow is not None:
+    #     name = input_file.name.replace(".parquet", "")
+    #     dataset = mlflow.data.from_pandas(df, source=input_file, name=name)
+    #     mlflow.log_input(dataset)
     return df
