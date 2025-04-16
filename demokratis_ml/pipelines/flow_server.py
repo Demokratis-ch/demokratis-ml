a7da949f4582bffab8cdc20aa268c06af96ae6e0
Vita Midori <hi@vitamidori.com>
2025-04-16 18:50:32 +0200

    Enable flow scheduling in flow_server.py

--- demokratis_ml/pipelines/flow_server.py
+++ demokratis_ml/pipelines/flow_server.py
@@ -1,12 +1,15 @@
 """Serve all our flows - this script is typically run inside a Docker container that has access to our Prefect server.
 
-The value of the ``STORE_DATAFRAMES_REMOTELY`` environment variable is used to set the ``store_dataframes_remotely``
-parameter of the flow(s).
+- The value of the ``STORE_DATAFRAMES_REMOTELY`` environment variable is used to set the ``store_dataframes_remotely``
+  parameter of the flow(s).
+- The ``CRON_PREPROCESS_CONSULTATION_DOCUMENTS`` environment variable, if set, is used to schedule the
+  ``preprocess-consultation-documents`` flow. The variable must be a valid cron string (e.g. "0 9 * * *").
 """
 
 import os
 
 import prefect
+import prefect.client.schemas.schedules
 
 from demokratis_ml.pipelines import preprocess_consultation_documents
 
@@ -19,5 +22,13 @@ if __name__ == "__main__":
             "publish": False,
             "store_dataframes_remotely": store_dataframes_remotely,
         },
+        schedules=[
+            prefect.client.schemas.schedules.CronSchedule(
+                cron=os.environ["CRON_PREPROCESS_CONSULTATION_DOCUMENTS"],
+                timezone="Europe/Zurich",
+            )
+        ]
+        if "CRON_PREPROCESS_CONSULTATION_DOCUMENTS" in os.environ
+        else None,
     )
     prefect.serve(preprocess_consultation_documents_deployment)
