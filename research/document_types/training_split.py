0c856656e3ace9758051ccc02116479882c93dcd
Vita Midori <hi@vitamidori.com>
2025-05-19 10:48:36 +0200

    Change how we split and use input data: construct the test set from manually labelled OpenParlData documents only and the train set from Fedlex, some rule-labelled OPD documents, and the rest of manually labelled OPD documents.

---
+++ research/document_types/training_split.py
@@ -0,0 +1,84 @@
+import logging
+
+import pandas as pd
+import sklearn.model_selection
+
+from research.document_types import document_title_rule_model
+
+
+def train_test_split(
+    df: pd.DataFrame,
+    random_state: int,
+    test_size: float,
+    include_rule_labels_in_training: set[str],
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Split the input dataframe (documents + extra features + embeddings) into a training and test set.
+
+    All Fedlex documents are used for training. OpenParlData documents are used as follows:
+    - Documents that already have labels must have been manually labelled by Demokratis staff; this high-quality
+      data is split into training and test sets according to the `test_size` parameter.
+    - Some documents with labels assigned by the rule-based model are used for training: only classes
+      listed in `include_rule_labels_in_training` are used as those are vetted to be reliable.
+    - The remaining OpenParlData documents are unlabelled and therefore discarded.
+
+    The resulting dataframes have the same columns as the input dataframe, but the `document_type` column is
+    guaranteed to be non-null. In addition, the `document_type_label_source` column is added to indicate
+    the source of the label. The possible values are:
+    - "fedlex": documents from Fedlex
+    - "rule": documents from OpenParlData with labels assigned by the rule-based model
+    - "manual": documents from OpenParlData that were manually labelled by Demokratis staff
+    """
+    logger = logging.getLogger("train_test_split")
+
+    df = df.copy()
+    df["document_type_label_source"] = ""
+
+    # All Fedlex documents are for training
+    df_fedlex = df.loc[df["document_source"] == "fedlex"]
+    df_fedlex.loc[:, "document_type_label_source"] = "fedlex"
+
+    df_openparldata = df.loc[df["document_source"] == "openparldata"]
+
+    # From the unlabelled OpenParlData, we use some rule-based labels for training
+    df_openparldata_unlabelled = df_openparldata.loc[df_openparldata["document_type"].isna()]
+    rule_labels = document_title_rule_model.predict(df_openparldata_unlabelled)
+    rule_labels = rule_labels[rule_labels.isin(include_rule_labels_in_training)]
+    df_openparldata_rules = df_openparldata.loc[rule_labels.index]
+    df_openparldata_rules.loc[rule_labels.index, "document_type"] = rule_labels
+    df_openparldata_rules.loc[rule_labels.index, "document_type_label_source"] = "rule"
+
+    # Manually labelled OpenParlData documents will be split into train and test
+    df_openparldata_manual = df_openparldata.loc[df_openparldata["document_type"].notna()]
+    df_openparldata_manual.loc[:, "document_type_label_source"] = "manual"
+    logger.info("Number of manually labelled openparldata documents: %d", len(df_openparldata_manual))
+    splitter = sklearn.model_selection.StratifiedShuffleSplit(
+        n_splits=1,
+        test_size=test_size,
+        random_state=random_state,
+    )
+    train_index, test_index = next(splitter.split(X=df_openparldata_manual, y=df_openparldata_manual["document_type"]))
+    df_openparldata_manual_train = df_openparldata_manual.iloc[train_index]
+    df_openparldata_manual_test = df_openparldata_manual.iloc[test_index]
+
+    # Concatenate all training data
+    df_train = pd.concat([df_fedlex, df_openparldata_rules, df_openparldata_manual_train], ignore_index=True)
+    logger.info(
+        "Training set: %d fedlex + %d rule labels + %d manual labels = %d",
+        len(df_fedlex),
+        len(df_openparldata_rules),
+        len(df_openparldata_manual_train),
+        len(df_train),
+    )
+    # Only some manually labelled OpenParlData documents will be used for testing
+    df_test = df_openparldata_manual_test
+    logger.info("Test set: %d manual labels", len(df_test))
+
+    # Integrity checks
+    assert not df_train["document_id"].duplicated().any(), "Train set must not contain duplicates"
+    assert not df_test["document_id"].duplicated().any(), "Test set must not contain duplicates"
+    assert not (set(df_train["document_id"]) & set(df_test["document_id"])), "Train and test sets must not overlap"
+    assert df_train["document_type"].notna().all(), "Train set must not contain null document types"
+    assert df_test["document_type"].notna().all(), "Test set must not contain null document types"
+
+    return df_train, df_test
